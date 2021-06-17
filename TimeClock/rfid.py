ea898d6ff7400be01950371fb86ff3f801faff19
Adrian <adriansandovalvargas@gmail.com>
2021-06-17 17:49:48 -0400

    threading

--- TimeClock/rfid.py
+++ TimeClock/rfid.py
@@ -2,55 +2,7 @@ import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import time
 import threading
-# from flask import redirect, url_for
-
-# reader = SimpleMFRC522()
-
-# def write(val):
-#     try:
-#         print("Place your tag to write")
-#         reader.write(val)
-#         print("Written")
-#     finally:
-#         GPIO.cleanup()
-
-# def read():
-#     try:
-#         while True:
-#             id, text = reader.read()
-#             print(id)
-#             print(text)
-#             # return redirect(url_for('home.login'))
-#             time.sleep(5)
-#     finally:
-#             GPIO.cleanup()
-
-# class RFID(threading.Thread):
-# 	def __init__(self):
-# 		super().__init__()
-#         self.reader = SimpleMFRC522()
-
-# 	# This is whats get called when you do RFID().start()
-# 	def run(self):
-#         try:
-#             while True:
-#                 id, text = self.reader.read()
-#                 print(id)
-#                 print(text)
-#                 # return redirect(url_for('home.login'))
-#                 time.sleep(5)
-#         finally:
-#                 GPIO.cleanup()
-    
-#     def write(val):
-#         try:
-#             print("Place your tag to write")
-#             self.reader.write(val)
-#             print("Written")
-#         finally:
-#             GPIO.cleanup()
-
-# RFID().start()
+from flask import redirect, url_for, flash
 
 class RFID(threading.Thread):
     def __init__(self):
@@ -76,6 +28,7 @@ class RFID(threading.Thread):
                 id, text = self.reader.read()
                 print(id)
                 print(text)
+                flash(text, 'success')
                 # return redirect(url_for('home.login'))
                 time.sleep(5)
         except:
