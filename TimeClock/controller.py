871194d7a3c5f9edb74061cae281745c997d93f8
Adrian <adriansandovalvargas@gmail.com>
2021-06-17 13:45:43 -0400

    mfrc

--- TimeClock/controller.py
+++ TimeClock/controller.py
@@ -8,11 +8,11 @@ home = Blueprint('home', __name__)
 @home.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        read() 
+        RFID.read() 
         return render_template('index.html')
     else:
         render_template('index.html')
 
-def read():
-    x = threading.Thread(target=RFID.read(), daemon=True)
-    x.start()
+# def read():
+#     x = threading.Thread(target=RFID.read(), daemon=True)
+#     x.start()
