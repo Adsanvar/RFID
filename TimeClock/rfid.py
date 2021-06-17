e1da2a7134e37479686b79ef20ea7d6e3231c88b
Adrian <adriansandovalvargas@gmail.com>
2021-06-17 12:16:07 -0400

    read

--- TimeClock/rfid.py
+++ TimeClock/rfid.py
@@ -4,13 +4,21 @@ from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
 
-def write(val):
-    try:
-        print("Place your tag to write")
-        reader.write(val)
-        print("Written")
-    finally:
-        GPIO.cleanup()
+# def write(val):
+#     try:
+#         print("Place your tag to write")
+#         reader.write(val)
+#         print("Written")
+#     finally:
+#         GPIO.cleanup()
+
+try:
+    val = "ADRIAN SANDOVAL-VARGAS"
+    print("Place your tag to write")
+    reader.write(val)
+    print("Written")
+finally:
+    GPIO.cleanup()
 
 # def read():
 #     try:
