480d218941cf30632d94ba5de32c22f8423acb37
Adrian <adriansandovalvargas@gmail.com>
2021-06-17 15:00:39 -0400

    mfrc

--- TimeClock/controller.py
+++ TimeClock/controller.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, flash, Blueprint, session, redirect, url_for
+import TimeClock.rfid as RFID
+import threading
+from multiprocessing import Process
 
 home = Blueprint('home', __name__)
 
@@ -6,6 +9,16 @@ home = Blueprint('home', __name__)
 @home.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return render_template('index.html')
+        # read()
+        # p = Process(target=RFID.read())
+        # p.daemon = True
+        # p.start() 
+        # target=RFID.read()
+        read()
+        return render_template('index.html', isActive=True)
     else:
         render_template('index.html')
+
+def read():
+    x = threading.Thread(target=RFID.read())
+    x.start()
