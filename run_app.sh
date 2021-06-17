c5ff3f22793a61f14d0eb36f0f40009804590e40
Adrian <adriansandovalvargas@gmail.com>
2021-06-17 16:28:47 -0400

    threading

--- run_app.sh
+++ run_app.sh
@@ -3,5 +3,6 @@ export FLASK_APP=TimeClock
 export FLASK_ENV=development
 export DEBUG=1
 export FLASK_RUN_PORT=5005
-flask run --no-reload
+flask run
+# flask run --no-reload
 # flask run -h 192.168.1.109
