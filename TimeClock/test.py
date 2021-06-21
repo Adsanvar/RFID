961aa454da532d238e7939aada30eb3dce8636eb
Adrian <adriansandovalvargas@gmail.com>
2021-06-21 17:13:32 -0400

    parsing data into template

--- TimeClock/test.py
+++ TimeClock/test.py
@@ -101,6 +101,7 @@ def loadOptions(window, payload):
 def start_server():
     app.run(host='0.0.0.0', port=5000)
 
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/<string:data>', methods=['GET', 'POST'])
 def index(data=None):
