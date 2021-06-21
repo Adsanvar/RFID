a72f54d0fe71d0df57c1109f70aaf877e286027c
Adrian <adriansandovalvargas@gmail.com>
2021-06-21 14:13:26 -0400

    webview

--- TimeClock/test.py
+++ TimeClock/test.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash, Blueprint, session, redirect, url_for, current_app
+import threading
+# from . import thread
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+import time
 import webview
 import sys
 import threading
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html', read = False)
+    else:
+        return render_template('index.html', read = False)
 
 def start_server():
     app.run(host='0.0.0.0', port=5000)
