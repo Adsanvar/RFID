1017f5450bc72b51410ac1ca54b2644cf75c9896
Adrian <adriansandovalvargas@gmail.com>
2021-06-22 20:28:01 -0400

    Modal

--- TimeClock/test.py
+++ TimeClock/test.py
@@ -124,16 +124,11 @@ def loadOptions(window, payload):
         {
             title: name,
             html: `
-            <input class="inp-cbx" id="no-lunch" type="checkbox"/>
-            <label class="cbx" for="no-lunch"><span>
-                <svg width="12px" height="10px">
-                <use xlink:href="#check"></use>
-                </svg></span><span>Sin Almuerzo</span></label>
-            <!-- <svg class="inline-svg">
-            <symbol id="check" viewbox="0 0 12 10">
-                <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
-            </symbol>
-            </svg> -->`,
+            <input  type="checkbox" name="no-lunch" id="no-lunch-cbx"/>
+            <span class="checkboxtext">
+            Sin Almuerzo
+            </span>
+            `,
             width: 600,
         })
     }
