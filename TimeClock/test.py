9cc1ffbe6bfb772abc775681edc0afc0c049ee6d
Adrian <adriansandovalvargas@gmail.com>
2021-06-22 20:23:19 -0400

    Modal

--- TimeClock/test.py
+++ TimeClock/test.py
@@ -129,11 +129,11 @@ def loadOptions(window, payload):
                 <svg width="12px" height="10px">
                 <use xlink:href="#check"></use>
                 </svg></span><span>Sin Almuerzo</span></label>
-            /*<svg class="inline-svg">
+            <!-- <svg class="inline-svg">
             <symbol id="check" viewbox="0 0 12 10">
                 <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
             </symbol>
-            </svg>*/`,
+            </svg> -->`,
             width: 600,
         })
     }
