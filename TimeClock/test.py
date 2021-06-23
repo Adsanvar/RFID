337ed56eb573d088762a85966efe54b83a186c7e
Adrian <adriansandovalvargas@gmail.com>
2021-06-22 21:25:03 -0400

    Modal

--- TimeClock/test.py
+++ TimeClock/test.py
@@ -124,6 +124,7 @@ def loadOptions(window, payload):
         swalBtnOkBootstrap.fire(
         {
             title: name,
+            icon: info,
             html: `
             <div class="big margin">
                 <input type="checkbox" name="no-lunch-cbx" id="no-lunch-cbx" /> 
