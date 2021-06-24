4fe9248d6ebff59853dab9a49a0e0c52d5d07667
Adrian <adriansandovalvargas@gmail.com>
2021-06-24 12:45:21 -0400

    api call

--- TimeClock/test.py
+++ TimeClock/test.py
@@ -115,10 +115,15 @@ def getserial():
   return cpuserial
 
 def validateFob(payload):
-    headers= {'content-type': 'application/json'}
-    data = json.dumps(payload)
-    res = requests.get(api_url+"validateFob", data=data, headers=headers)
-    print(res.text)
+    try:
+        headers= {'content-type': 'application/json'}
+        data = json.dumps(payload)
+        res = requests.get(api_url+"validateFob", data=data, headers=headers)
+        
+        print(res.json('message'))
+    except Exception as e:
+        print(e)
+        return jsonify(message='Error')
 
 def loadOptions(window, payload):
     # url = base_url + json.dumps(payload)
@@ -252,10 +257,14 @@ def index(data=None):
 @app.route('/clockin/<string:data>')
 def clockin(data=None):
     if data != None:
-        headers= {'content-type': 'application/json'}
-        res = requests.get(api_url+"clockin", data=data, headers=headers)
-        # return jsonify(message='Success')
-        return res.text
+        try:
+            headers= {'content-type': 'application/json'}
+            res = requests.get(api_url+"clockin", data=data, headers=headers)
+            # return jsonify(message='Success')
+            return res.text
+        except Exception as e:
+            print(e)
+            return jsonify(message='Error')
     else:
         return jsonify(message='Error No Data')
 
