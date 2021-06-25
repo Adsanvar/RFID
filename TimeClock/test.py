5152caa71d077c08a10c6627f90bacadceb2c373
Adrian <adriansandovalvargas@gmail.com>
2021-06-25 13:04:51 -0400

    write

--- TimeClock/test.py
+++ TimeClock/test.py
@@ -416,7 +416,7 @@ def writer(data=None):
             # print("_stop.set(): ")
             # readthread._stop()
             # print("isAlive(): ", readthread.isAlive())
-            write = threading.thread(target=write(data))
+            write = threading.Thread(target=write(data))
             
             return jsonify(message='Success')
         except Exception as e:
