8f22ee35ef6404204d875e4a38ca47fa69736be5
Adrian <adriansandovalvargas@gmail.com>
2021-06-29 13:19:08 -0400

    imports

--- TimeClock/server.py
+++ TimeClock/server.py
@@ -10,7 +10,7 @@ import threading
 import json
 import os
 import requests
-from reader import Reader
+from reader import ReadThread as Reader
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123456789'
