1626797aef1fa7fe14622e960f4baa43a1ce79cd
Adrian <adriansandovalvargas@gmail.com>
2021-07-01 11:23:01 -0400

    daemon write thread stop method

--- run_app.sh
+++ run_app.sh
@@ -6,3 +6,13 @@ export FLASK_RUN_PORT=5000
 flask run
 # flask run --no-reload
 # flask run -h 192.168.1.65
+
+timer=5
+
+echo "Application will start in: "
+while (($timer !=0))
+do
+    echo $timer
+    sleep 1s
+    timer=$((timer-1))
+done
