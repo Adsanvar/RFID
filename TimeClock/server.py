f1ec869747ba64a79f38e11d310ca68e4911bce3
Adrian <adriansandovalvargas@gmail.com>
2021-07-01 01:13:21 -0400

    daemon write thread stop method

--- TimeClock/server.py
+++ TimeClock/server.py
@@ -331,7 +331,7 @@ if __name__ == '__main__':
     # t.daemon = True
     # t.start()
     # startReadThread(True)
-    readthread.start()
+    readthread.run()
     # writethread.setWriteFlag(False)
     # writethread.start()
     
