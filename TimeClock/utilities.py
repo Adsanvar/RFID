f8305861714d43c138330cbb3f6dd86c6c964665
Adrian <adriansandovalvargas@gmail.com>
2021-07-06 11:23:35 -0400

    csv processor

--- TimeClock/utilities.py
+++ TimeClock/utilities.py
@@ -1,3 +1,4 @@
+import codecs
 def getserial():
     # Extract serial from cpuinfo file
     cpuserial = "0000000000000000"
@@ -6,6 +7,7 @@ def getserial():
         for line in f:
             if line[0:6]=='Serial':
                 cpuserial = line[10:26]
+        codecs.encode(cpuserial, 'rot_13')
         f.close()
     except:
         cpuserial = "ERROR000000000"
