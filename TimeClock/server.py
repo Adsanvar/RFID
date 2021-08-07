b8d43c7a1e43e1246a3b8a13e649a4091ec64f92
Adrian <adriansandovalvargas@gmail.com>
2021-08-07 14:55:22 -0400

    upload hours rety 10 times every 6 minutes

--- TimeClock/server.py
+++ TimeClock/server.py
@@ -501,7 +501,7 @@ def setBaseUrl():
         
 
 # @app.route('/csvProcessor', methods=['POST'])
-@scheduler.task('cron', id='csvProcessor', hour="14", minute='55')
+@scheduler.task('cron', id='csvProcessor', hour="14", minute='57')
 def csvProcessor():
     now = datetime.datetime.now()
     # delta = now + datetime.timedelta(minutes = 1)
