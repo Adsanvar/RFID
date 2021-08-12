337b5b34db1e33ca7e4ccebd79bd28d70f067cee
Adrian <adriansandovalvargas@gmail.com>
2021-08-12 15:17:28 -0400

    time

--- TimeClock/server.py
+++ TimeClock/server.py
@@ -510,7 +510,7 @@ def setBaseUrl():
         
 
 # @app.route('/csvProcessor', methods=['POST'])
-@scheduler.task('cron', id='csvProcessor', hour="15", minute='14')
+@scheduler.task('cron', id='csvProcessor', hour="15", minute='19')
 def csvProcessor():
     now = datetime.datetime.now()
     # delta = now + datetime.timedelta(minutes = 1)
