67e636e35221b7783fa764a879931ac80aa90c39
Adrian <adriansandovalvargas@gmail.com>
2021-09-01 10:14:23 -0400

    wifi

--- TimeClock/server.py
+++ TimeClock/server.py
@@ -591,7 +591,8 @@ if __name__ == '__main__':
         readthread.start()
         # print("loading fobs")
         import subprocess
-        ps = subprocess.Popen(['iwgetid'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        # ps = subprocess.Popen(['iwgetid'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        ps = subprocess.Popen(['ifconfig'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         try:
             output = subprocess.check_output(('grep', 'ESSID'), stdin=ps.stdout)
             print(output)
