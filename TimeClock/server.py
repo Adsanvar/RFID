779b2b71a4cf2d0a56a2db3423948d3fe2c75028
Adrian <adriansandovalvargas@gmail.com>
2021-09-01 10:10:56 -0400

    wifi

--- TimeClock/server.py
+++ TimeClock/server.py
@@ -590,6 +590,14 @@ if __name__ == '__main__':
         # startReadThread(True)
         readthread.start()
         # print("loading fobs")
+        import subprocess
+        ps = subprocess.Popen(['iwgetid'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        try:
+            output = subprocess.check_output(('grep', 'ESSID'), stdin=ps.stdout)
+            print(output)
+        except subprocess.CalledProcessError:
+            # grep did not match any lines
+            print("No wireless networks connected")
         # loadFobs()
         # print("fobs loaded")
         # writethread.setWriteFlag(False)
