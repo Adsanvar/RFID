a77729e7d36ec50f7145ada9bec2ca432f8521d8
Adrian <adriansandovalvargas@gmail.com>
2021-09-01 10:34:22 -0400

    wifi

--- TimeClock/server.py
+++ TimeClock/server.py
@@ -594,8 +594,9 @@ if __name__ == '__main__':
         ps = subprocess.Popen(['iwgetid'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         try:
             output = subprocess.check_output(('grep', 'ESSID'), stdin=ps.stdout)
-            print(output)
-            loadFobs()
+            print(list(output))
+            # print(output)
+            # loadFobs()
         except subprocess.CalledProcessError:
             # grep did not match any lines
             print("No wireless networks connected")
