b7d71a19dc644763591525b042a7f56fe010f0ca
Adrian <adriansandovalvargas@gmail.com>
2022-05-04 23:30:04 -0400

    dbc classes

--- TimeClock/server.py
+++ TimeClock/server.py
@@ -208,7 +208,7 @@ def index():
     else:
         print("INDEX:")
         print(dbc.getFobs())
-        tc = db.TimeClock(fobid=123, date=datetime.datetime.now(), clock_in=datetime.datetime.now(), nolunch=False)
+        tc = dbc.TimeClock(fobid=123, date=datetime.datetime.now(), clock_in=datetime.datetime.now(), nolunch=False)
         print(tc)
         # database.createTimeclock(tc)
         # print(database.getTimeclockRowById(123))
