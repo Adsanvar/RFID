a36a499a5beb8cbb37706ad0684301471675b740
Adrian <adriansandovalvargas@gmail.com>
2022-05-04 22:15:24 -0400

    time clock

--- TimeClock/home.py
+++ TimeClock/home.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request, flash, Blueprint, session, redirect, url_for, jsonify, send_file, Response
 
-home_page = Blueprint('home_page', __name__, template_folder='templates')
+home = Blueprint('home', __name__, template_folder='templates')
 
-@home_page.route('/', methods=['GET'])
+@home.route('/', methods=['GET'])
 def index(): 
-    # return render_template('index.html')
-    return "HELLO"
+    return render_template('index.html')
