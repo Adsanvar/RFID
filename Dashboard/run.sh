6f9ec1cc2f7480d175e7abebdb5319d3683dc36e
Adrian <adriansandovalvargas@gmail.com>
2022-05-11 09:56:38 -0400

    prod env

--- Dashboard/run.sh
+++ Dashboard/run.sh
@@ -1,6 +1,6 @@
-#-!/usr/bin/bash
+#!/usr/bin/bash
 export FLASK_APP=Dashboard
 export FLASK_DEBUG=1
 export FLASK_ENV=Development
-# flask run -h 192.168.1.65 -p 5005
-flask run
+flask run -h 192.168.1.65 -p 5005
+# flask run
